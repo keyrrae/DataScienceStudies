e40ca4abf5ea6f86e5a2ae1f6a7e6f120232fe1c
keyrrae <xuan-wang@outlook.com>
2015-08-03 23:41:24 +0800

    sma

--- USD-RMB-prediction.R
+++ USD-RMB-prediction.R
@@ -1,1 +1,12 @@
 exchangeRate <- read.csv("./USDtoRMB.csv",header=TRUE, sep=",")
+exchangeRateTS <- ts(exchangeRate$Mid, frequency=365, start=c(2014,1))
+plot.ts(exchangeRateTS)
+
+install.pakcages("TTR")
+library("TTR")
+
+exchangeRateSMA5 <- SMA(exchangeRateTS, n=5)
+plot.ts(exchangeRateSMA5)
+
+exchangeRateSMA10 <- SMA(exchangeRateTS, n=10)
+plot.ts(exchangeRateSMA10)
